92e77a7300a7d5166398f49d386172214bc2bf6b
hugocotoflorez <hugocoto100305@gmail.com>
2024-03-15 17:07:53 +0100

    Update by update_dotfiles

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ZSH=".zshrc .zprofile"
-CONFIG=".config/yay/ .config/nvim/init.lua .config/nvim/lua/ .config/nvim/after/ .config/rofi/ .config/bspwm/ .config/picom/ .config/sxhkd/ .config/polybar/ .config/gtk-2.0/ .config/gtk-3.0/  .config/alacritty/alacritty.toml"
+CONFIG=".config/yay/ .config/nvim/vim_cheatsheet.md .config/nvim/init.lua .config/nvim/lua/ .config/nvim/after/ .config/rofi/ .config/bspwm/ .config/picom/ .config/sxhkd/ .config/polybar/ .config/gtk-2.0/ .config/gtk-3.0/  .config/alacritty/alacritty.toml"
 CFORMAT=".clang-format"
 OTHERS2=".fehbg .gtkrc-2.0"
 OTHERS=".bash_profile .xinitrc README.md .screenlayout/* .scripts/* update_dotfiles.sh"
