f8d0a6671aaabb950b433a1074501c7bdde8c928
hugocotoflorez <hugocoto100305@gmail.com>
2024-12-09 00:44:19 +0100

    TOGGLE TOUCHPAD SCRIPT

---
+++ toggle_touchpad.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+file="/sys/bus/pci/devices/0000:01:00.0/power/control"
+content=$(<"$file")
+
+if [[ "$content" == "on" ]]; then
+    echo "auto" > "$file"
+
+else
+    echo "on" > "$file"
+fi
--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -2,6 +2,6 @@
 
 cd ~/dotfiles
 git add .
-git commit -m "AutoUpdate (round + bottom waybar)"
+git commit -m "AutoUpdate"
 git push origin main
 
