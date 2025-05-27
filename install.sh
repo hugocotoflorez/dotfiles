e552925d58b0ba3fcfa3bf0b260a1292739bdb86
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-05-27 13:05:34 +0200

    adding yay to experimental installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+exist() { command -v "$1" >/dev/null 2>&1; }
+
+if ! exist yay; then
+        sudo pacman -S --needed git base-devel
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si
+fi
+
 sudo ./sysupdate.sh
 
 yay -S --noconfirm --needed `cat ./installed-packages.txt`
