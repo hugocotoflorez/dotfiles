084ec7c27eaea1343d4142d676fe24934032fee3
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-07-03 18:16:44 +0200

    doing right

--- install.sh
+++ install.sh
@@ -10,7 +10,8 @@ if ! exist yay; then
 fi
 
 git submodule update --init nvim
-sudo ./sysupdate.sh
+sudo bash -c "which reflector && reflector -c fr -n 10 > /etc/pacman.d/mirrorlist"
+sudo pacman -Syyu --noconfirm
 
 yay -S --noconfirm --needed `cat ./installed-packages.txt`
 
