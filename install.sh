b61f4207c7ebee826360bbbc999d8dcfaaf43a36
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-07-03 20:32:14 +0200

    u

--- install.sh
+++ install.sh
@@ -2,24 +2,6 @@
 
 exist() { command -v "$1" >/dev/null 2>&1; }
 
-if ! exist yay; then
-        sudo pacman -S --needed git base-devel
-        git clone https://aur.archlinux.org/yay.git
-        cd yay
-        makepkg -si
-fi
-
-git submodule update --init nvim
-sudo pacman -Syyu --noconfirm
-
-cat ./installed-packages.txt | xargs -P 4 -I {} bash -c 'yay -S --needed --noconfirm "{}" || echo "{}" >> failed-packages.txt'
-
-rm ~/.config -rf
-mkdir ~/.config
-
-./deploy.sh ./MANIFEST
-sudo ./deploy.sh ./MANIFEST
-
 function clone_install() {
         if [ -d "$1" ]; then
                 rm -rf "$1"
@@ -32,9 +14,31 @@ function clone_install() {
         fi
 }
 
-mkdir -p .local/bin
+if ! exist yay; then
+        sudo pacman -S --needed git base-devel
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si
+fi
+
+sudo ls -s default.conf /etc/keyd
+sudo ls -s pacman.conf /etc
+
+git submodule update --init nvim
+sudo pacman -Syyu --noconfirm
+
+rm ~/.* -rf
+
+mkdir -p ~/.config
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/share/applications
+
+./deploy.sh ./MANIFEST
+sudo ./deploy.sh ./MANIFEST
 
 clone_install tetris
 clone_install todo
 clone_install hfetch
+
+yay -S --needed --noconfirm `cat ./installed-packages.txt`
 
