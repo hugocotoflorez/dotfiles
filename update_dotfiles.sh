f7b3246f8b608d58986102f3fda3452142c86622
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-07-04 15:28:51 +0200

    Auto Update

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cd ~/dotfiles/nvim
 
 pacman -Qe | cut -d' ' -f1 > installed-packages.txt
 
+SCRIPT_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+cd $SCRIPT_DIR
 git add .
 git commit -m "Auto Update"
 git push origin main
